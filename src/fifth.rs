6a30390a698c819a4bb4de5cc49adb07f25d3011
Smit Soni <atsmtat@Smit-MacBook.local>
2023-09-29 19:24:08 -0700

    Implement fast unsafe queue using raw pointers

--- src/fifth.rs
+++ src/fifth.rs
@@ -10,49 +10,151 @@ pub struct Node<T> {
     next: Link<T>,
 }
 
-pub type Link<T> = Option<Box<Node<T>>>;
+pub type Link<T> = *mut Node<T>;
 
 impl<T> List<T> {
     pub fn new() -> Self {
         List {
-            head: None,
+            head: ptr::null_mut(),
             tail: ptr::null_mut(),
         }
     }
 
     pub fn push(&mut self, val: T) {
-        let mut new_tail = Box::new(Node { val, next: None });
-
-        let raw_tail: *mut _ = &mut *new_tail;
-        if !self.tail.is_null() {
-            unsafe {
-                (*self.tail).next = Some(new_tail);
+        unsafe {
+            let raw_tail = Box::into_raw(Box::new(Node {
+                val,
+                next: ptr::null_mut(),
+            }));
+            if !self.tail.is_null() {
+                (*self.tail).next = raw_tail;
+            } else {
+                self.head = raw_tail;
             }
-        } else {
-            self.head = Some(new_tail);
+            self.tail = raw_tail;
         }
-
-        self.tail = raw_tail;
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|head| {
-            match head.next {
-                Some(new_head) => {
-                    self.head = Some(new_head);
+        unsafe {
+            if !self.head.is_null() {
+                let result = Box::from_raw(self.head);
+                self.head = result.next;
+                if self.head.is_null() {
+                    self.tail = self.head;
                 }
-                None => {
-                    self.tail = ptr::null_mut();
-                }
+                Some(result.val)
+            } else {
+                None
             }
-            head.val
-        })
+        }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        unsafe { self.head.as_ref().map(|node| &node.val) }
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unsafe { self.head.as_mut().map(|node| &mut node.val) }
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        self.into_iter()
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        self.into_iter()
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<T> IntoIterator for List<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter(self)
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.take().map(|node| {
+                self.next = node.next.as_ref();
+                &node.val
+            })
+        }
+    }
+}
+
+impl<'a, T> IntoIterator for &'a List<T> {
+    type Item = &'a T;
+    type IntoIter = Iter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        unsafe {
+            Iter {
+                next: self.head.as_ref(),
+            }
+        }
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.take().map(|node| {
+                self.next = node.next.as_mut();
+                &mut node.val
+            })
+        }
+    }
+}
+
+impl<'a, T> IntoIterator for &'a mut List<T> {
+    type Item = &'a mut T;
+    type IntoIter = IterMut<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        unsafe {
+            IterMut {
+                next: self.head.as_mut(),
+            }
+        }
     }
 }
 
 impl<T> Default for List<T> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop().is_some() {}
     }
 }
 
@@ -86,5 +188,90 @@ mod tests {
         assert_eq!(list.pop(), Some(6));
         assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(Some(1), iter.next());
+        assert_eq!(Some(2), iter.next());
+        assert_eq!(Some(3), iter.next());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+
+        let mut iter = list.iter();
+        assert_eq!(Some(&1), iter.next());
+        assert_eq!(Some(&2), iter.next());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(Some(&mut 1), iter.next());
+        assert_eq!(Some(&mut 2), iter.next());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn mix_it() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(Some(1), list.pop());
+        assert_eq!(Some(&2), list.peek());
+
+        list.pop();
+        list.push(4);
+
+        assert_eq!(Some(&3), list.peek());
+        if let Some(val) = list.peek_mut() {
+            *val *= 100;
+        }
+        assert_eq!(Some(&300), list.peek());
+        assert_eq!(Some(300), list.pop());
+        list.push(5);
+
+        for val in &mut list {
+            *val *= 100;
+        }
+
+        let mut iter = list.iter();
+        assert_eq!(Some(&400), iter.next());
+        assert_eq!(Some(&500), iter.next());
+        assert_eq!(None, iter.next());
+        assert_eq!(None, iter.next());
+
+        assert_eq!(Some(400), list.pop());
+        if let Some(val) = list.peek_mut() {
+            *val *= 10;
+        }
+        assert_eq!(Some(&5000), list.peek());
+
+        list.push(6000);
+
+        {
+            let empty: List<i32> = List::new();
+            assert_eq!(None, empty.peek());
+            // drop empty
+        }
     }
 }
